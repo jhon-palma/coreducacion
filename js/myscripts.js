acdaae57307c770c64d2dc9f5c9dc29b1b487867
jhon1946 <jhon-palma@upc.edu.co>
2020-10-30 01:38:16 -0500

    'video'

---
+++ js/myscripts.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('.venobox-video').venobox({
+    autoplay: true,
+    bgcolor: 'rgba(241,90,41, 0.8)',
+    border: '5px',
+    titleattr: 'Prueba',
+    spinner: 'three-bounce',
+    spinColor: '#f15a29',
+  });
+
+})
